52e3fefa46f56604835b8deafe70f1722355e713
Josh <15219491+jkaps9@users.noreply.github.com>
2025-08-12 17:46:47 -0400

    remove mobile menu javascript

--- src/index.js
+++ src/index.js
@@ -1,9 +1,2 @@
 import "./styles.css";
 import "./components.css";
-
-const menu = document.querySelector(".menu");
-const hamburger = document.querySelector(".hamburger");
-
-hamburger.addEventListener("click", () => {
-  menu.classList.toggle("active");
-});
